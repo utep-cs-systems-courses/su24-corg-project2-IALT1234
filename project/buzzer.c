78576bd4f674869be5096d24166bc4b2c84c2567
Ivan Luna <ialunatorre@miners.utep.edu>
2024-07-06 22:16:48 -0600

    third?

---
+++ project/buzzer.c
@@ -0,0 +1,32 @@
+#include <msp430.h>
+
+#include "../demos/timerLib/libTimer.h"
+
+#include "../demos/11-buzzer/buzzer.h"
+
+
+
+void buzzer_init(){
+
+  timerAUpmode();/* used to drive speaker */
+
+  P2SEL2 &= ~(BIT6 | BIT7);
+
+  P2SEL &= ~BIT7;
+
+  P2SEL |= BIT6;
+
+  P2DIR = BIT6;/* enable output to speaker (P2.6) */
+
+}
+
+
+
+void buzzer_set_period(short cycles)
+{
+
+  CCR0 = cycles;
+
+  CCR1 = cycles >> 1;/* one half cycle */
+
+}
